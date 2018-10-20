471352cbee31701b062b1c99340135558f23387a
Jamison J <39608261+jbjohnson1278@users.noreply.github.com>
2018-10-20 14:28:44 -0400

    Update python/main.py

--- python/main.py
+++ python/main.py
@@ -8,6 +8,11 @@ from models import Interest
 from models import Image
 import os.path
 
+the_jinja_environment = jinja2.Environment(
+    loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions = [],
+    autoescape = True)
+
 jinja_current_directory = jinja2.Environment(
     loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions = ['jinja2.ext.autoescape'],
